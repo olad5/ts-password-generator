ced38a4b9ecb908d6efaa9dcbee67b8607b48a74
Francis Shonubi <shonubioladipo@gmail.com>
2022-02-22 17:22:38 +0100

    refactor: add proper class methods
    - added proper class methods using OOP concepts.
    - separated generation of character pools into methods

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,51 @@
-const asciiChars: number[] = Array.from(Array(94)).map((e, i) => i + 33);//unicode character set of password characters
-const characters: string[] = asciiChars.map((x) => String.fromCharCode(x));// generates the  characters from the unicode character set
-
-class PasswordGenerator {
+// NOTE: The Private variables and functions are labelled that way  becauses they are
+// all just relevant within the context of the class
+class RandomPasswordGenerator {
+    private lettersCharacterPool: string[] = []
+    private numbersCharacterPool: string[] = []
     length: number
     amtOfLetters: number
-    excludeList: string
 
-    constructor(length: number, amtOfLetters: number, excludeList: string) {
+    constructor(length: number, amtOfLetters: number, exclusionString: string) {
+        this.generateCharacterPools(exclusionString) // Whenever the class is initialized, this function is called and populates the characterPool
         this.length = length
         this.amtOfLetters = amtOfLetters
-        this.excludeList = excludeList
+    }
+
+    private generateCharacterPools(exclusionString: string) {
+        const asciiChars: number[] = Array.from(Array(94)).map((e, i) => i + 33); // unicode character set of password characters
+        const _characters = asciiChars.map((x) => String.fromCharCode(x)); // generates the  characters from the unicode character set
+        const characterPool = _characters.filter((c) => exclusionString.includes(c) === false)
+        this.lettersCharacterPool = characterPool.filter((c) => {
+            let charCode = c.charCodeAt(0)
+            return (charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)// the filter method just need a boolean value
+        })
+        this.numbersCharacterPool = characterPool.filter(x => this.lettersCharacterPool.indexOf(x) === -1)
+    }
+
+    private generateInclusionListFromCharacterPool(pool: string[], length: number) {
+        let resultingArray: string[] = []
+        for (let i = 0; i < length; i++) {
+            resultingArray.push(pool[Math.floor(Math.random() * pool.length)])
+        }
+        return resultingArray
+    }
+
+    private shuffleArray(arr: string[]): string[] {
+        return arr.sort(() => Math.random() - 0.5);
     }
 
     generate() {
-
-        let availableCharPool: string[] = characters.filter((c) => this.excludeList.includes(c) === false)
-
-        let availableCharPoolLetters: string[] = availableCharPool.filter((c) => {
-            let charCode = c.charCodeAt(0)
-            if ((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)) {
-                return c
-            }
-        })
-
-        let availableCharPoolSymbolsNumbers: string[] = availableCharPool.filter(x => availableCharPoolLetters.indexOf(x) === -1);
-
-
-        let resultingArray: string[] = []
-
-        for (let i = 0; i < this.amtOfLetters; i++) {
-            resultingArray.push(availableCharPoolLetters[Math.floor(Math.random() * availableCharPoolLetters.length)])
-        }
-
-        for (let i = 0; i < (this.length - this.amtOfLetters); i++) {
-            resultingArray.push(availableCharPoolSymbolsNumbers[Math.floor(Math.random() * availableCharPoolSymbolsNumbers.length)])
-        }
-
-
-        function shuffleArray(arr: string[]): string[] {
-            return arr.sort(() => Math.random() - 0.5);
-        }
-
-        let password: string = (shuffleArray(resultingArray).join(''))
+        // Because this function cannot be called without initializing the class, it's safe to do this.
+        const passwordValues = [
+            ...this.generateInclusionListFromCharacterPool(this.lettersCharacterPool, this.amtOfLetters),
+            ...this.generateInclusionListFromCharacterPool(this.numbersCharacterPool, this.length - this.amtOfLetters)
+        ]
+        let password: string = (this.shuffleArray(passwordValues).join(''))
         return password
 
     }
-
 }
 
-const password = new PasswordGenerator(20, 9, 'JNkAm*ASKAja')
+const password = new RandomPasswordGenerator(10, 4, 'JNkAm*ASKAja')
 console.log(password.generate());
